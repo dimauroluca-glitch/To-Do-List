a93f27469efcb85587d89c2dd292e37e1d498bfe
DimAURA <dimauro.luca@studenti.salesianisesto.it>
2026-06-22 14:18:50 +0200

    Update to-do.js

--- to-do.js
+++ to-do.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, 'nuova-cartella')));
+app.use(express.static(path.join(__dirname)));
 app.listen(PORT, () => {
     console.log(`=============================================`);
     console.log(` Server attivo con successo!`);
