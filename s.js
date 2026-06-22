d4db7778c13e6ae0b95d56ce54cbec0241acb3e3
DimAURA <dimauro.luca@studenti.salesianisesto.it>
2026-06-22 13:33:49 +0200

    Update s.js

--- s.js
+++ s.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, 'Nuova cartella')));
+app.use(express.static(path.join(__dirname, 'nuova_cartella')));
 app.get('/', (req, res) => {
     res.send('<h1>Il mio sito Node.js è online e funziona! 🚀</h1>');
 });
