95df5be2196ec172632512658e24441b149a6f51
DimAURA <dimauro.luca@studenti.salesianisesto.it>
2026-06-22 15:12:08 +0200

    Update to-do.js

--- to-do.js
+++ to-do.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.mongodb+srv:lucadimauro2009_db_user:BzqpaitwyKkNSmpk@cluster0.xezpfwy.mongodb.net/?appName=Cluster0;
+if (!MONGODB_URI) {
+    console.error("ATTENZIONE: MONGODB_URI non configurato nelle variabili d'ambiente!");
+} else {
+    mongoose.connect(MONGODB_URI)
+        .then(() => console.log(' Database MongoDB collegato con successo!'))
+        .catch(err => console.error(' Errore di connessione al Database:', err));
+}
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname)));
 app.listen(PORT, () => {
     console.log(`=============================================`);
